3bc04a852c04c60b355b5750c8d96dcf58ed3ef0
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-05-22 16:26:32 +0200

    pretty

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,11 @@
 Ingredient.destroy_all
 Cocktail.destroy_all
 Dose.destroy_all
+ingredients = %w(gingerbeer cucumber vodka lemon ice scotch water rum sugar limes gin bourbon cachaca)
+ingredients.each do |ingredient|
+    Ingredient.create(name: ingredient )
+end
 
-Ingredient.create(name: "ginger beer")
-Ingredient.create(name: "cucumber")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
 Dose.create(description: "2 slices of", cocktail_id: 1, ingredient_id: 2)
 Dose.create(description: "4 cl", cocktail_id: 1, ingredient_id: 3)
 Dose.create(description: "200 ml", cocktail_id: 1, ingredient_id: 1)
